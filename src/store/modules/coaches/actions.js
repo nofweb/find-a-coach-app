3216ded5b0157f16042007e73063936df8ea5f35
Napoleon Festus <nofwebdev@gmail.com>
2022-10-24 00:11:50 +0100

    First commit

---
+++ src/store/modules/coaches/actions.js
@@ -0,0 +1,55 @@
+export default {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
+    const coachData = {
+      firstName: data.first,
+      lastName: data.last,
+      description: data.desc,
+      hourlyRate: data.rate,
+      areas: data.areas,
+    };
+    const token = context.rootGetters.token;
+    const response = await fetch(
+      `${process.env.VUE_APP_FIREBASE_DB}/coaches/${userId}.json?auth=` + token,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
+
+    if (!response.ok) {
+      // error....
+    }
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId,
+    });
+  },
+
+  async loadCoaches(context) {
+    const response = await fetch(
+      `${process.env.VUE_APP_FIREBASE_DB}/coaches.json`
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to fetch');
+      throw error;
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+    context.commit('setCoaches', coaches);
+  },
+};
